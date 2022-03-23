15d294889d59c685166ea7bf3bcf68038c4d2ebb
Sunaga0709 <ts19980709@gmail.com>
2022-03-24 00:27:59 +0900

    create messages_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_120914) do
+ActiveRecord::Schema.define(version: 2022_03_23_152132) do
+
+  create_table "messages", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "provider", default: "email", null: false
